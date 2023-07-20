be05b7ab9b23df679dd480f521df4eda80960225
Paulo-Ribas <paulo13paulo423@gmail.com>
2023-07-20 16:25:57 -0300

    esperar

--- model/dateFuncions.js
+++ model/dateFuncions.js
@@ -2,7 +2,9 @@ class DateFunctions {
     remakeDate(dateNow) {
         let dateObject = new Date(dateNow)
         let day = dateObject.getDay()
+        console.log(day, dateObject.getDate())
         dateObject.setDate(day)
+        console.log(dateNow, dateObject.getTime(), dateObject.getDay())
         return dateObject
     }
 }
