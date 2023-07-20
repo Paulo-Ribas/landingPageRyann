11ae4d8a742f32e1ef441f97347471299c1906d7
Paulo-Ribas <paulo13paulo423@gmail.com>
2023-07-20 16:12:20 -0300

    testando

--- model/dateFuncions.js
+++ model/dateFuncions.js
@@ -3,7 +3,6 @@ class DateFunctions {
         let dateObject = new Date(dateNow)
         let day = dateObject.getDay()
         dateObject.setDate(day)
-        console.log(dateObject.getDate())
         return dateObject
     }
 }
