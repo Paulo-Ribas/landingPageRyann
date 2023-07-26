864d0bf31b97af6f03e9e014eab130c76ac02c35
Paulo-Ribas <paulo13paulo423@gmail.com>
2023-07-26 19:16:55 -0300

    qual o problema senhor

--- model/dateFuncions.js
+++ model/dateFuncions.js
@@ -2,7 +2,10 @@ class DateFunctions {
     remakeDate(dateNow) {
         let dateObject = new Date(dateNow)
         let day = dateObject.getDate()
+        console.log(day)
+        console.log(dateObject)
         dateObject.setDate(day + 1)
+        console.log(dateObject)
         return dateObject
     }
 }
